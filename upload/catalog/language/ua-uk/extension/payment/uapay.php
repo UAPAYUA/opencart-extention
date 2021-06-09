be9119855e2bed38b716c15193b0e8bf1e1fa74e
Kazbanov Oleksandr <kazbanov@ukr.net>
2021-06-09 12:17:41 +0300

    Create uapay.php

---
+++ upload/catalog/language/ua-uk/extension/payment/uapay.php
@@ -0,0 +1,17 @@
+<?php
+// Text
+$_['text_title']		= 'Онлайн оплата UaPay';
+$_['text_credit_card']	= 'Підтвердження платежу';
+$_['text_loading']		= 'Завантаження...';
+$_['text_pay_success']		= 'Замовлення було сплачено через UaPay. На сумму %1$s';
+
+$_['text_pay_auth']		= 'Платіж було заблоковано через UaPay. За для здійснення платежу змініть статус замовлення на ';
+$_['text_pay_void']		= 'Платіж було розблоковано через UaPay';
+
+$_['text_invoice_id']		= 'ID invoice(invoice_id)';
+
+$_['text_payment_id']		= 'ID payment(payment_id)';
+$_['text_order']		= 'Замовлення';
+$_['text_pay_refund']		= 'Замовлення було повернуто через UaPay';
+$_['text_pay_failure']		= 'Замовлення не сплачено через UaPay';
+$_['text_error']		= 'Заборонено змінювати статус на - ';
